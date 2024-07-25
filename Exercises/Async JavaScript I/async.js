8d39df6d269f4696d2e81e5b4aa429c2d169b703
lealem <birhanumelesse12@gmail.com>
2024-07-25 14:10:18 +0300

    assignment async js I

---
+++ Exercises/Async JavaScript I/async.js
@@ -0,0 +1,54 @@
+
+const fetchTodo = async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+        const data = await response.json()
+        console.log("Data: ", data)
+    } catch (err) {
+        console.error("ohhhps an error has occured: \n", err)   
+    }
+
+}
+fetchTodo();
+
+
+
+function callbackHell () {
+    function callback4 () {
+        console.log("This is callback 4")
+    }
+    function callback3 () {
+        callback4();
+    }
+    function callback2 (callback3) {
+        callback3(callback4);
+    }
+    function callback (callback2) {
+        callback2(callback3);
+    }
+    callback(callback2);
+}
+
+callbackHell();
+
+
+
+// Q #1 using promises
+const fetchData = new Promise((resolve, reject) => {
+    resolve(fetch("https://jsonplaceholder.typicode.com/todos/1"));  
+});
+const processData = (data) => {
+    return new Promise((resolve, reject) => {
+        resolve(data.json())
+    });
+};
+fetchData
+    .then(data => processData(data))
+    .then(jsonData => console.log(jsonData))
+    .catch(err => console.log(err))
+
+
+
+
+
+
